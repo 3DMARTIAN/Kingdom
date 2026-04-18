f6394ee32c5974a5dede2003f8450d19bc646b59
begeritos <72303665+3DMARTIAN@users.noreply.github.com>
2026-04-18 11:29:16 +0700

    Create kingdom.py

---
+++ kingdom.py
@@ -0,0 +1,12 @@
+def kingdom_info():
+    elements = [
+        "Castles with tall towers",
+        "Brave knights",
+        "Wise kings and queens",
+        "Vast lands and villages"
+    ]
+    for item in elements:
+        print(item)
+
+if __name__ == "__main__":
+    kingdom_info()
